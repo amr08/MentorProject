ab1b21f4782525c1930787df5018001c1b69c87b
Andrea R <amrdev08@gmail.com>
2016-09-01 21:28:02 -0400

    Thursday night Master branch Push

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,20 +1,50 @@
 $(document).ready(function(){
 
+
+
 ////FRONT END JS////////
 	$(window).scroll(function(){
 
 		$("#heading").css({"opacity" : 1-(($(this).scrollTop())/300)
+
+		
 		});
 
 		$("#title").css({"opacity" : 1-(($(this).scrollTop())/300)
+			
+			
 		});
+
 
 
 	});
 
 
-$("#menu").mouseover(function(){
-	$("#menu").append("<p> Drop Down Menu</p>")
+$(document).on("scroll", function() {
+
+	if ($(document).scrollTop() > 600){
+		$("#menu").fadeIn();
+		$("#menu").removeClass("hide");
+		$("#menu").addClass("show");
+
+	}
+
+	else {
+
+		$("#menu").removeClass("show");
+		$("#menu").addClass("hide");
+
+	}
+
+
+});	
+
+//dropdown
+
+$("#dropDown").hide();
+
+$("#menu").on("click", function(){
+	$("#dropDown").toggleClass("show");
 });
 //////END FRONT END JS///////
 
